350d7d672c37a8cac0dbfdaebb549f6e12edb0e9
Sam Bragg <sam@sams-mbp-2022.home>
2023-10-04 15:42:07 +0100

    Revert "Work on linkedin scraping"
    This reverts commit b31e3385535906f5f4b0a2247d1004f784eafd97.

--- main.ts
+++ main.ts
@@ -36,38 +36,19 @@ function exportToCsv(filename: string, rows: any[][]): void {
     }
 }
 
-interface LinkedInProfile {
-    fsdProfile: string
-    searchTerm?: string,
-    linkedInProfileUrl?: string,
-    firstName?: string,
-    lastName?: string,
-    fullName?: string,
-    title?: string,
-    location?: string,
-    isPremium?: boolean,
-    summary?: string,
-}
+declare var members_list:string[][]; // Will store facebook group members
+window.members_list = window.members_list || [[
+    'Profile Id',
+    'Full Name',
+    'ProfileLink',
+    'Bio',
+    'Image Src',
+    'Groupe Id',
+    'Group Joining Text',
+    'Profile Type'
+]]
 
-// declare var profiles_list:string[][]; // Will store linkedin profile
-declare var profiles_list:Map<string, LinkedInProfile>; // Will store linkedin profile
-window.profiles_list = window.profiles_list || new Map();
-
-// [[
-//     'First Name',
-//     'Last Name',
-//     'Headline',
-//     'ProfileLink',
-//     // 'Full Name',
-//     // 'Title',
-//     // 'Location',
-//     // 'Profile Link',
-//     // 'Summary',
-//     // 'Is Premium',
-//     // 'Connection Degree'
-// ]]
-
-// Add a Download button to export parsed records into a CSV file
+// Add a Download button to export parsed member into a CSV file
 function buildCTABtn(): HTMLElement{
     const canvas = document.createElement('div')
     const canvasStyles = [
@@ -84,12 +65,12 @@ function buildCTABtn(): HTMLElement{
     const btn = document.createElement('div')
     const btnStyles = [
         'position: absolute;',
-        'bottom: 80px;',
+        'bottom: 30px;',
         'right: 130px;',
         'color: white;',
         'min-width: 150px;',
-        'background: blue;',
-        'border-radius: 8px',
+        'background: var(--primary-button-background);',
+        'border-radius: var(--button-corner-radius);',
         'padding: 0px 12px;',
         'cursor: pointer;',
         'font-weight:600;',
@@ -104,7 +85,7 @@ function buildCTABtn(): HTMLElement{
 
     const downloadText = document.createTextNode('Download\u00A0')
     const numberSpan = document.createElement("span");
-    numberSpan.setAttribute('id', 'linkedin-scraper-number-tracker')
+    numberSpan.setAttribute('id', 'fb-group-scraper-number-tracker')
     numberSpan.textContent = "0";
     const memberText = document.createTextNode('\u00A0members')
 
@@ -113,41 +94,8 @@ function buildCTABtn(): HTMLElement{
     btn.appendChild(memberText)
 
     btn.addEventListener('click', function() {
-        const timestamp = new Date().toISOString();
-
-        const profileToArray: string[][] = [[
-            'Search Terms',
-            'LinkedInUrl',
-            'First Name',
-            'Last Name',
-            'Full Name',
-            'Title',
-            'Location',
-            'Is Premium',
-            'Summary'
-        ]];
-
-        function textOrEmpty(value){
-            return value || ''
-        };
-
-        window.profiles_list.forEach((profile, index)=>{
-            // Only keep profile with a linkedIn profile URL
-            if(profile.linkedInProfileUrl){
-                profileToArray.push([
-                    profile.searchTerm,
-                    profile.linkedInProfileUrl,
-                    textOrEmpty(profile.firstName),
-                    textOrEmpty(profile.lastName),
-                    textOrEmpty(profile.fullName),
-                    textOrEmpty(profile.title),
-                    textOrEmpty(profile.location),
-                    profile.isPremium ? 'True' : 'False',
-                    textOrEmpty(profile.summary)
-                ])
-            }
-        })
-        exportToCsv(`linkedInProfilesExport-${timestamp}.csv`, profileToArray)
+        const timestamp = new Date().toISOString()
+        exportToCsv(`groupMemberExport-${timestamp}.csv`, window.members_list)
     });
 
     canvas.appendChild(btn);
@@ -156,201 +104,94 @@ function buildCTABtn(): HTMLElement{
     return canvas;
 }
 
-function isProfile1(entity){
-    return entity['$type'] === "com.linkedin.voyager.dash.search.EntityResultViewModel" && 
-    !!entity['navigationUrl'] && entity['navigationUrl'].indexOf('linkedin.com/in/') !== -1;
-}
-
-function isProfile2(entity){
-    return entity['$type'] === "com.linkedin.voyager.dash.identity.profile.Profile"
-    //  &&  !!entity['publicIdentifier']
-}
-
-function cleanLinkedInUrl(sourceLinkedInUrl: string): string {
-    const cleanedUrl = new URL(sourceLinkedInUrl);
-    cleanedUrl.search = ''
-    cleanedUrl.hash = ''
-    return cleanedUrl.toString();
-}
-
-function findProfileUsingFSDProfile(fsdProfile: string){
-    for (let [key, value] of window.profiles_list.entries()) {
-        if (value.fsdProfile && value.fsdProfile === fsdProfile) {
-            return key;
-        }
-    }
-    return null
-}
-
-function processResponse(dataGraphQL: any, searchTerm?: string): void{
+function processResponse(dataGraphQL: any): void{
     // Only look for Group GraphQL responses
     let data: any;
-    if(dataGraphQL?.included){
+    if(dataGraphQL?.data?.group){
         // Initial Group members page
-        data = dataGraphQL?.included;
+        data = dataGraphQL.data.group;
+    } else if(dataGraphQL?.data?.node?.__typename === 'Group'){
+        // New members load on scroll
+        data = dataGraphQL.data.node;
     } else {
         // If no group members, return fast
         return;
     }
 
-    const profileObjs = data.filter((entity)=>{
-        return isProfile1(entity) || isProfile2(entity)
-    })
-
-    if(profileObjs.length === 0){
+    let membersEdges: Array<any>;
+    // Both are used (new_forum_members seems to be the new way)
+    if(data?.new_members?.edges){
+        membersEdges = data.new_members.edges;
+    }else if(data?.new_forum_members?.edges){
+        membersEdges = data.new_forum_members.edges;
+    }else if(data?.search_results?.edges){
+        membersEdges = data.search_results.edges;
+    }else{
         return
     }
 
-    function parseProfile1(profileNode): LinkedInProfile{
-        const profileLink = profileNode?.navigationUrl;
+    const membersData = membersEdges.map(memberNode=>{
+        // Member Data
+        const {
+            id,
+            name,
+            bio_text,
+            url,
+            profile_picture,
+            __isProfile:profileType
+        } = memberNode.node
 
-        if(!profileLink){
-            return null
-        };
+        // Group Joining Info
+        const joiningText = memberNode?.join_status_text?.text || memberNode?.membership?.join_status_text?.text;
 
-        const entityUrn = profileNode?.entityUrn
-        let fsdProfile: string;
-        const regExResult = entityUrn.match(/fsd_profile:(?<profile>(\w+))/);
-        if(regExResult && regExResult.groups && regExResult.groups['profile']) {
-            fsdProfile = regExResult.groups['profile']
-        }
+        // Facebook Group Id
+        const groupId = memberNode.node.group_membership?.associated_group.id
 
-        if(!fsdProfile){
-            return null;
-        }
-        // "urn:li:fsd_entityResultViewModel:(urn:li:fsd_profile:ACoAACbapPsBIXhvyKSmbAWi_6qcr0hu7HGHzqg,SEARCH_SRP,DEFAULT)"
-
-        const fullName = profileNode?.title?.text;
-        const title = profileNode?.primarySubtitle?.text;
-        const location = profileNode?.secondarySubtitle?.text;
-        const summary = profileNode?.summary?.text;
-        let isPremium = false;
-        if(profileNode?.badgeIcon){
-            isPremium = true;
-        }
-        // // profileNode?.badgeIcon?.attributes[0]?.detailData?.icon.toLowerCase().indexOf('premium') !== -1;
-        // const connectionDegree = profileNode?.badgeText?.accessibilityText;
-
-        const toReturn: LinkedInProfile = {
-            fsdProfile: fsdProfile,
-            linkedInProfileUrl: cleanLinkedInUrl(profileLink),
-            isPremium: isPremium
-        }
-        if(searchTerm){
-            toReturn.searchTerm = searchTerm
-        }
-        if(fullName){
-            toReturn.fullName = fullName;
-        }
-        if(title){
-            toReturn.title = title;
-        }
-        if(location){
-            toReturn.location = location;
-        }
-        if(summary){
-            toReturn.summary = summary;
-        }
-        return toReturn
-    }
-
-    function parseProfile2(profileNode): LinkedInProfile {
-        const entityUrn = profileNode?.entityUrn
-        let fsdProfile: string;
-        if(!entityUrn){
-            return null
-        }
-        const regExResult = entityUrn.match(/fsd_profile:(?<profile>(\w+))/);
-        if(regExResult && regExResult.groups && regExResult.groups['profile']) {
-            fsdProfile = regExResult.groups['profile']
-        }
-        if(!fsdProfile){
-            return null;
-        }
-
-        const toReturn: LinkedInProfile = {
-            fsdProfile: fsdProfile
-        }
-
-        const publicIdentifier = profileNode?.publicIdentifier;
-        const firstName = profileNode?.firstName;
-        const lastName = profileNode?.lastName;        
-        const headline = profileNode?.headline;
-        if(publicIdentifier){
-            toReturn.linkedInProfileUrl =`https://www.linkedin.com/in/${publicIdentifier}`
-        }
-        if(firstName){
-            toReturn.firstName = firstName;
-        }
-        if(lastName){
-            toReturn.lastName = lastName;
-        }
-        if(headline){
-            toReturn.title = headline;
-        }
-        if(searchTerm){
-            toReturn.searchTerm = searchTerm
-        }
-        
-        return toReturn
-    }
-
-
-    profileObjs.forEach(profileNode=>{
-        console.log(profileNode)
-        let profile: LinkedInProfile;
-        if(isProfile1(profileNode)){
-            profile = parseProfile1(profileNode)
-            console.log(`Profile1 ${profile.fullName} - ${profile.firstName} - ${profile.lastName}`)
-        }else if(isProfile2(profileNode)){
-            profile = parseProfile2(profileNode)
-            console.log(`Profile2 ${profile.fullName} - ${profile.firstName} - ${profile.lastName}`)
-        }else{
-            throw new Error('Invalid profile')
-        }
-
-        if(!profile){
-            return;
-        }
-
-        console.log(`fsdProfile: ${profile.fsdProfile}`)
-        const existingProfile = window.profiles_list.get(profile.fsdProfile)
-        if(existingProfile){
-            console.log('Merge')
-            window.profiles_list.set(profile.fsdProfile, {
-                ...existingProfile,
-                ...profile
-            })
-        }else{
-            window.profiles_list.set(profile.fsdProfile, profile)
-        }
+        return [
+            id,
+            name,
+            url,
+            bio_text?.text || '',
+            profile_picture?.uri || '',
+            groupId,
+            joiningText || '',
+            profileType
+        ]
     })
 
+    window.members_list.push(...membersData)
+
     // Update member tracker counter
-    const tracker = document.getElementById('linkedin-scraper-number-tracker')
+    const tracker = document.getElementById('fb-group-scraper-number-tracker')
     if(tracker){
-        const cleanMap = new Map([...window.profiles_list].filter(([k, v])=>{
-            return !!v.linkedInProfileUrl
-        }))
-        tracker.textContent = cleanMap.size.toString()
+        tracker.textContent = window.members_list.length.toString()
     }
 }
 
 
 function parseResponse(dataRaw: string): void{
-    // // be sure we are on search url
-    // if(window.location.pathname !== "/search/results/people/"){
-    //     return;
-    // }
-
-    // const keywordReg = window.location.search.match(/keywords=(?<search>.+?)(?=&)/)
-    // const keyword = keywordReg && keywordReg.groups && keywordReg.groups['search']
-
     let dataGraphQL: Array<any> = [];
     try{
         dataGraphQL.push(JSON.parse(dataRaw))
     }catch(err){
-        console.error('Fail to parse API response', err);
+        // Sometime Facebook return multiline response
+        const splittedData = dataRaw.split("\n");
+
+        // If not a multiline response
+        if(splittedData.length<=1){
+            console.error('Fail to parse API response', err);
+            return;
+        }
+
+        // Multiline response. Parse each response
+        for(let i=0; i<splittedData.length;i++){
+            const newDataRaw = splittedData[i];
+            try{
+                dataGraphQL.push(JSON.parse(newDataRaw));
+            }catch(err2){
+                console.error('Fail to parse API response', err);
+            }
+        }
     }
 
     for(let j=0; j<dataGraphQL.length; j++){
@@ -362,44 +203,12 @@ function main(): void {
     buildCTABtn()
 
     // Watch API calls to find GraphQL responses to parse
-    const matchingUrl = '/voyager/api/graphql';
-    const regstart = /start:(?<start>\d+)/ig
-    const increasePagination = 40;
-    
-    const open = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function(){
-        if(arguments[1].indexOf('/voyager/api/graphql')!=-1){
-            const newArgs = arguments;
-            const currentUrl = arguments[1];
-            regstart.lastIndex = 0;
-            const result = regstart.exec(currentUrl);
-
-            if(result){
-                const currentStart = result?.groups?.start;
-                if(currentStart){
-                    const currentPage = parseInt(currentStart)/10;
-                    const newStart = currentPage*(10+increasePagination)
-                    newArgs[1] = currentUrl.replace(`start:${currentStart}`, `count:${10+increasePagination},start:${newStart}`)
-                    return open.apply(this, newArgs);
-                }
-            }
-        }
-        return open.apply(this, arguments);
-    }
-    const send = XMLHttpRequest.prototype.send;
+    const matchingUrl = '/api/graphql/';
+    let send = XMLHttpRequest.prototype.send;
     XMLHttpRequest.prototype.send = function() {
         this.addEventListener('readystatechange', function() {
             if (this.responseURL.includes(matchingUrl) && this.readyState === 4) {
-                const reader: FileReader = new FileReader();
-                reader.onloadend = (e) => {
-                    try{
-                        // console.log(reader.result)
-                        parseResponse(reader.result as string);
-                    }catch(err){
-                        console.error(err)
-                    }
-                };
-                reader.readAsText(this.response);
+                parseResponse(this.responseText);
             }
         }, false);
         send.apply(this, arguments);
